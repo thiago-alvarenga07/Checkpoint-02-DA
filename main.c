b011d11ce1f1fc32c7fb74fcc1b2ad3787011355
Dimeendes <rm565509@fiap.com.br>
2025-05-12 09:46:11 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <locale.h>
+
+int fibonacci (){ //Função que efetua calculos para gerar a sequência de Fibonacci
+int a = 0, b = 1, Fibo[50], N; //Declaração de variáveis
+printf("Digite a quantidade de termos da sequência de Fibonacci (1 a 50): ");
+scanf("%d", &N);
+printf("Sequência de Fibonacci: %d %d ", a, b);
+for(int i = 1; i < N; i++){ //o "for" é utilizado neste caso para repetir o calculo até a posição desejada pelo usuário
+        Fibo[i] = a + b;
+        a = b;
+        b = Fibo[i];
+        printf("%d ", Fibo[i]);
+}
+}
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+setlocale(LC_ALL, "Portuguese");
+
+int escolha;
+
+setlocale(LC_ALL, "Portuguese");
+printf("===== MENU DE EXERCÍCIOS =====\n");
+printf("1 - Sequência de Fibonacci;\n");
+printf("2 - Fatoriais;\n");
+printf("3 - Verificar Palíndromo;\n");
+printf("4 - Verificar Substring.\n");
+printf("Escolha o exercício: ");
+scanf("%d", &escolha);
+
+switch(escolha){ //Utiliza-se a função switch case para
+    case 1:
+        fibonacci();
+
+}
+//EXERCICIO 1 - SEQUÊNCIA DE FIBONACCI    
+fibonacci(); //Declarando função para realizar os cálculos do exercicio
 }
