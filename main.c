f573c1b35548c575386079790367a159014022c8
thiago-alvarenga07 <rm562695@fiap.com.br>
2025-05-13 13:30:24 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <ctype.h>
 
+//Exercicio 1 - Fibonacci
 void fibonacci (){ //Função que efetua calculos para gerar a sequência de Fibonacci
 int a = 0, b = 1, Fibo[50], N; //Declaração de variáveis
 printf("Digite a quantidade de termos da sequência de Fibonacci (1 a 50): ");
@@ -18,6 +19,72 @@ for(int i = 1; i < N; i++){ //o "for" é utilizado neste caso para repetir o cal
 }
 }
 
+//Exercicio 2 - Fatoriais com vetores
+void fatoriais()
+{
+    int i ,num;
+
+    int fatorial[num];
+    printf("Digite um numero inteiro (1 a 20): ");
+    scanf("%d", &num);
+
+    printf("Fatoriais:\n");
+    for (i = 0; i < num; i++)
+    {
+        if(i == 0) //if usado pois a formula usada no else não consegue calcular o fatorial da primeira posição
+        {
+          fatorial[i] = 1;
+        }
+        else
+        {
+            fatorial[i] = (i + 1) * fatorial[i - 1]; //Formula utilizada para calcular o valor do fatorial de acordo com a posição no vetor
+        }
+        printf("%d! = %d\n", i + 1, fatorial[i]);
+    }
+
+}
+
+//Exercicio 3 - palindromo
+
+void palindromo() {
+
+  int verificacao, i, tamanho, tamanhofor;
+
+  char palavra[101];
+
+  printf("Digite uma string: ");
+  scanf("%s", palavra);
+
+  tamanho = strlen(palavra);
+
+  tamanhofor =  tamanho;
+  tamanho--;
+
+  for(i = 0; i != tamanhofor / 2; i++)
+  {
+    if (palavra[i] == palavra[tamanho])
+    {
+      verificacao = 1;
+    }
+    else
+    {
+        verificacao = 0;
+    }
+    tamanho--;
+  }
+  if (verificacao == 1)
+  {
+      printf("A palavra é um palindromo");
+  }
+  else
+  {
+      printf("A palavra não é um palindromo");
+  }
+}
+
+
+
+//Exercicio 4 - verificar substring
 void substring(){
 char palavra1[101], palavra2[101]; //declarando as variaveis para cada string, podendo conter até 100 caracteres em cada
 printf("Digite a primeira string: ");
@@ -57,80 +124,20 @@ printf("Escolha o exercício: ");
 scanf("%d", &escolha);
 
 switch(escolha){ //Utiliza-se a função switch case para
-    case 1:
-        fibonacci();
+        case 1:
+                fibonacci();
+                break;
+        case 2:
+                fatoriais();
+                break;
+        case 3:
+                palindromo();
+                break;
+        case 4:
+                substring();
+                break;
+        default:
+                printf("Opção ínvalida");
 
-}
-//EXERCICIO 1 - SEQUÊNCIA DE FIBONACCI    
-fibonacci(); //Declarando função para realizar os cálculos do exercicio
-}
-
-
-//Exercicio 2 - Fatoriais com vetores
-int main()
-{
-    int i ,num;
-
-    setlocale(LC_ALL, "Portuguese");
-
-    int fatorial[num];
-    printf("Digite um numero inteiro (1 a 20): ");
-    scanf("%d", &num);
-
-    printf("Fatoriais:\n");
-    for (i = 0; i < num; i++)
-    {
-        if(i == 0) //if usado pois a formula usada no else não consegue calcular o fatorial da primeira posição
-        {
-          fatorial[i] = 1;
-        }
-        else
-        {
-            fatorial[i] = (i + 1) * fatorial[i - 1]; //Formula utilizada para calcular o valor do fatorial de acordo com a posição no vetor
-        }
-        printf("%d! = %d\n", i + 1, fatorial[i]);
-    }
-
-}
-
-//Exercicio 3 - palindromo
-
-int main() {
-
-  setlocale(LC_ALL, "Portuguese");
-
-  int verificacao, i, tamanho, tamanhofor;
-
-  char palavra[101];
-
-  printf("Digite uma string: ");
-  scanf("%s", palavra);
-
-  tamanho = strlen(palavra);
-
-  tamanhofor =  tamanho;
-  tamanho--;
-
-  for(i = 0; i != tamanhofor / 2; i++)
-  {
-    if (palavra[i] == palavra[tamanho])
-    {
-      verificacao = 1;
-    }
-    else
-    {
-        verificacao = 0;
-    }
-    tamanho--;
-  }
-  if (verificacao == 1)
-  {
-      printf("A palavra é um palindromo");
-  }
-  else
-  {
-      printf("A palavra não é um palindromo");
-  }
-}
 
 
